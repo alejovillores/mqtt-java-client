24d637be3a97fe7daee96b80aab9a92246b15f48
alejovillores <alejovillores@gmail.com>
2022-02-23 17:52:34 -0300

    feat: Subscriptions manage bytes

--- src/main/java/model/MqttSubscription.java
+++ src/main/java/model/MqttSubscription.java
@@ -1,8 +1,12 @@
 package model;
 
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
 public class MqttSubscription {
     private String topic;
     private int qos;
+    final int QOS_LENGTH = 3;
 
     public MqttSubscription(String topic, int qos) {
         this.qos = qos;
@@ -16,5 +20,26 @@ public class MqttSubscription {
 
     private boolean sameTopic(String topic) {
         return topic.equals(this.topic);
+    }
+
+    public int completeLength() {
+        return (this.topic.length() + QOS_LENGTH);
+    }
+
+    public ArrayList<Byte> packetIDBytes() {
+        ArrayList<Byte> bytes = new ArrayList<>();
+
+        short len = (short) this.topic.length();
+        bytes.add((byte)(len >> 8));
+        bytes.add((byte)(len & 0x00ff));
+        for (byte b : this.topic.getBytes(StandardCharsets.UTF_8)){
+            bytes.add(b);
+        }
+
+        return bytes;
+    }
+
+    public Byte qosBytes() {
+        return (byte) this.qos;
     }
 }
