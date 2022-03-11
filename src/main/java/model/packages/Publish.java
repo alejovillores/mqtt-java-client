53c0e2cce8b039ba96e93273e65d70b34490a387
alejovillores <alejovillores@gmail.com>
2022-03-11 12:06:04 -0300

    refactor: from interface to abstract

--- src/main/java/model/packages/Publish.java
+++ src/main/java/model/packages/Publish.java
@@ -3,20 +3,51 @@ package model.packages;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public interface Publish {
-    int getId();
+public abstract class Publish {
+    private String payload;
+    private String topicName;
+    private int qos;
 
-    boolean isRetained();
+    public Publish(String payload, String topicName,int qos) {
+        this.payload = payload;
+        this.topicName = topicName;
+        this.qos = qos;
+    }
 
-    boolean isDuplicated();
+    public int getId(){
+        return -1;
+    };
 
-    Byte flagByte();
+    public boolean isRetained(){
+        return false;
+    };
 
-    Collection<Byte> lengthAlgorithm();
+    public boolean isDuplicated(){
+        return false;
+    };
 
-    Collection<Byte> variableHeaderBytes();
+    public abstract Byte flagByte();
 
-    Collection<Byte> payloadBytes();
+    public Collection<Byte> lengthAlgorithm(){
+        int  x = (2 + this.payload.length() + this.topicName.length());
+        if (qos > 0) x += 2;
 
-    ArrayList<Byte> toBytes();
+        ArrayList<Byte> b = new ArrayList<>();
+        while (x > 0) {
+            int encoded_byte = (x % 128);
+            x /= 128;
+
+            if (x > 0) {
+                encoded_byte |= 128;
+            }
+            b.add((byte) encoded_byte);
+        }
+        return b;
+    };
+
+    public abstract Collection<Byte> variableHeaderBytes();
+
+    public abstract Collection<Byte> payloadBytes();
+
+    public abstract ArrayList<Byte> toBytes();
 }
